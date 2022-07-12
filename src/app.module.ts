ccf2930a3156b1a1f5626c8b5ed22e8a35f2da22
Abdelrahman Omran <abdelrahman.omran@climedo.de>
2022-07-12 19:30:44 +0200

    fix database connection issues on heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { Booking } from './domain/entities/Booking';
     ConfigModule.forRoot(),
     HttpModule,
     TypeOrmModule.forRoot({
+      type: 'postgres',
       url: process.env.DATABASE_URL,
       entities: [Hotel, Booking],
       synchronize: true,
