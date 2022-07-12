85e516a52bb73f4815c61a0535d1390ebf86a6c5
Abdelrahman Omran <abdelrahman.omran@climedo.de>
2022-07-12 19:16:37 +0200

    Use url to connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,7 @@ import { Booking } from './domain/entities/Booking';
     ConfigModule.forRoot(),
     HttpModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       entities: [Hotel, Booking],
       synchronize: true,
     }),
